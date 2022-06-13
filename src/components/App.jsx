ee716aced2917cebc9cb988919eac1d740d14eb2
jokerav <50701756+jokerav@users.noreply.github.com>
2022-06-13 09:31:05 +0300

    123

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,51 @@
 import { Component } from 'react';
+import { nanoid } from 'nanoid';
 import Filter from './Filter/Filter';
 import ContactForm from './ContactsForm/ContactForm';
+import ContactList from './ContactList/ContactList';
 
 export class App extends Component {
   state = {
     contacts: [],
     filter: '',
+  };
+  componentDidMount() {
+    const contacts = localStorage.getItem('contacts');
+    this.setState({ contacts: JSON.parse(contacts) });
+  }
+  componentDidUpdate(prevState) {
+    const { contacts } = this.state;
+    if (prevState.contacts !== contacts) {
+      localStorage.setItem('contacts', JSON.stringify(contacts));
+    }
+  }
+  isNameInPhonebook = name => {
+    const nameInLowerCase = name.toLowerCase();
+    for (const contact of this.state.contacts) {
+      if (contact.name.toLowerCase() === nameInLowerCase) {
+        return true;
+      }
+    }
+    return false;
+  };
+
+  addContact = contact => {
+    const { name } = contact;
+    if (!this.isNameInPhonebook(name)) {
+      contact.id = nanoid(5);
+      this.setState(prevState => prevState.contacts.push(contact));
+    } else {
+      alert(`${name} is already in contacts`);
+    }
+  };
+  onFilterChange = e => {
+    const { value } = e.currentTarget;
+    this.setState({ filter: value });
+  };
+  deleteContact = id => {
+    this.setState(prevState => ({
+      contacts: prevState.contacts.filter(contact => contact.id !== id),
+    }));
   };
   render() {
     return (
