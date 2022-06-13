1cf4dedf6877388081db03924ed044abe1e5fe5f
jokerav <50701756+jokerav@users.noreply.github.com>
2022-06-13 09:21:16 +0300

    12

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,25 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        fontSize: 22,
-        color: '#010101',
-      }}
-    >
-      <h1>Phonebook</h1>
-    </div>
-  );
-};
+import { Component } from 'react';
+import Filter from './Filter/Filter';
+import ContactForm from './ContactsForm/ContactForm';
+
+export class App extends Component {
+  state = {
+    contacts: [],
+    filter: '',
+  };
+  render() {
+    return (
+      <div
+        style={{
+          fontSize: 22,
+          color: '#010101',
+        }}
+      >
+        <h1>Phonebook</h1>
+        <ContactForm onSubmit={this.addContact} />
+        <Filter value={this.state.filter} onChange={this.onFilterChange} />
+        <h2>Contacts</h2>
+      </div>
+    );
+  }
+}
