e1af551195556db5f59caaee88792dfb1877092c
jokerav <50701756+jokerav@users.noreply.github.com>
2022-06-13 09:11:37 +0300

    f

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,15 +2,11 @@ export const App = () => {
   return (
     <div
       style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
+        fontSize: 22,
+        color: '#010101',
       }}
     >
-      React homework template
+      <h1>Phonebook</h1>
     </div>
   );
 };
